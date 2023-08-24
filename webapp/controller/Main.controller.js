4712659933d7b824e4dca5731f74e506195673f0
Omri Cohen <omricohen@gmail.com>
2023-08-24 14:04:46 +0300

    Initial Commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define(
+  [
+    "com/sample/project/controller/BaseController",
+  ],
+  function (
+    BaseController,
+  ) {
+    "use strict";
+
+    return BaseController.extend("com.sample.project.controller.Main", {
+      onInit: function () {
+      },
+
+      onExit: function() {
+      }
+
+
+    });
+  }
+);
